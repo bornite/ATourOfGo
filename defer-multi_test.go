68a79b2919c94300001804625bdfc2818cf40428
bornite <tmkoikee@gmail.com>
2019-07-07 00:40:41 +0900

    Add defer-multi.go

---
+++ defer-multi_test.go
@@ -0,0 +1,18 @@
+package main
+
+func ExampleMain() {
+    main()
+    // Output:
+    // counting
+    // done
+    // 9
+    // 8
+    // 7
+    // 6
+    // 5
+    // 4
+    // 3
+    // 2
+    // 1
+    // 0
+}
