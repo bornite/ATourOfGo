e3ea50552ba16c3d6dfcef01cb354352b2cfca01
bornite <tmkoikee@gmail.com>
2020-01-05 14:32:16 +0900

    Add slices.go, slices-pointers.go

---
+++ slices-pointers_test.go
@@ -0,0 +1,10 @@
+package main
+
+func ExampleMain() {
+    main()
+    // Output:
+    // [John Paul George Ringo]
+    // [John Paul] [Paul George]
+    // [John XXX] [XXX George]
+    // [John XXX George Ringo]
+}
