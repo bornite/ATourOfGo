0b21f2ee9f641e6de9ff81373d2c9b991f77482a
bornite <tmkoikee@gmail.com>
2020-01-29 21:26:22 +0900

    Add maps.go

---
+++ map-literals_test.go
@@ -0,0 +1,7 @@
+package main
+
+func ExampleMain() {
+    main()
+    // Output:
+    // map[Bell Labs:{40.68433 -74.39967} Google:{37.42202 -122.08408}]
+}
---
+++ maps_test.go
@@ -0,0 +1,7 @@
+package main
+
+func ExampleMain() {
+    main()
+    // Output:
+    // {40.68433 -74.39967}
+}
