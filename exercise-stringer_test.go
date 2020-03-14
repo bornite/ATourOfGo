0c1b9e2d78abb9a8475413a11ca96190b538d2d5
bornite <tmkoikee@gmail.com>
2020-03-14 13:10:50 +0900

    Add exercise-stringer.go

---
+++ exercise-stringer_test.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+	"testing"
+)
+
+func captureStdout(f func()) string {
+	r, w, err := os.Pipe()
+	if err != nil {
+		panic(err)
+	}
+
+	stdout := os.Stdout
+	os.Stdout = w
+
+	f()
+
+	os.Stdout = stdout
+	w.Close()
+
+	var buf bytes.Buffer
+	io.Copy(&buf, r)
+
+	return buf.String()
+}
+
+func TestString(t *testing.T) {
+
+	loopback := IPAddr{127, 0, 0, 1}
+
+	out := captureStdout(func() {
+		fmt.Printf("%v\n", loopback)
+	})
+
+	if out != "127.0.1.1\n" {
+		t.Errorf("test1 : Unexpected string: %s", out)
+	}
+}
