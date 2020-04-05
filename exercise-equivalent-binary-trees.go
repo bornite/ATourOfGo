38808cecbddd678ef5e784e47fbefcde30143a24
bornite <tmkoikee@gmail.com>
2020-04-05 19:09:34 +0900

    :sparkles: Implement Same() function

--- exercise-equivalent-binary-trees.go
+++ exercise-equivalent-binary-trees.go
@@ -18,7 +18,32 @@ func Walk(t *tree.Tree, ch chan int) {
 
 // Same determines whether the trees
 // t1 and t2 contain the same values.
-//func Same(t1, t2 *tree.Tree) bool
+func Same(t1, t2 *tree.Tree) bool {
+    ch1 := make(chan int)
+    ch2 := make(chan int)
+
+    go func() {
+        Walk(t1, ch1)
+        close(ch1)
+    }()
+    go func() {
+        Walk(t2, ch2)
+        close(ch2)
+    }()
+
+    for {
+        v1, close1 := <- ch1
+        v2, close2 := <- ch2
+        if v1 != v2 {
+            return false
+        }
+        if !close1 && !close2 {
+            return true
+        }
+    }
+
+    return false
+}
 
 func main() {
 	// Call Walk() function
@@ -31,4 +56,8 @@ func main() {
 	for n := range ch {
 		fmt.Println(n)
 	}
+
+       // Call Same() function
+       fmt.Println("tree1 eq tree1 = ", Same(tree.New(1), tree.New(1)))
+       fmt.Println("tree1 eq tree2 = ", Same(tree.New(1), tree.New(2)))
 }
